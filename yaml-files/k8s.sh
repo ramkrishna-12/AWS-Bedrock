14ac8231bf06d4547757d01120badebd218be556
ramkrishna <rupambarmanxyz@gmail.com>
2026-06-29 23:44:33 +0530

    edit

--- yaml-files/k8s.sh
+++ yaml-files/k8s.sh
@@ -5,3 +5,7 @@ kubectl create secret generic openstack-credentials \
   --from-literal=OS_PASSWORD=mypassword \
   --from-literal=OS_USER_DOMAIN_NAME=Default \
   --from-literal=OS_PROJECT_DOMAIN_NAME=Default
+
+# This command creates a Kubernetes secret named "openstack-credentials" that contains the OpenStack authentication credentials. The secret can be used by pods in the cluster to authenticate with OpenStack services.
+# Make sure to replace the placeholder values (https://openstack.example.com:5000/v3, myproject, myuser, mypassword) with your actual OpenStack credentials before running the command.
+# kubectl get secret openstack-credentials -o yaml
